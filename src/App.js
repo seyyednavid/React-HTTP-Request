fb007e4710bfbd0171748aee1193ec061380cc67
Seyyednavid Hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-06-26 11:56:22 +0100

    Add useState to show loading text befor fetching

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMoviesHandler = async () => {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films");
     const data = await response.json();
     const transformedMovies = data.results.map((movieData) => {
@@ -16,6 +18,7 @@ function App() {
         id: movieData.episode_id,
       };
     });
+    setIsLoading(false);
     return setMovies(transformedMovies);
   };
 
@@ -25,7 +28,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>There is no movies</p>}
+        {isLoading && <p>Loading ...</p>}
       </section>
     </React.Fragment>
   );
